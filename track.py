b10ba3b0c75cb4a6b810d89617d866343ca86085
M3nti0nl3ss <ayoub-sifo@live.fr>
2020-09-07 00:46:45 +0100

    First commit

---
+++ track.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+import sys
+
+URL = "https://www.1tracking.net/en/detail?nums=" + str(sys.argv[1])
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+browser = webdriver.Chrome(options=options)
+browser.get(URL)
+sleep(10)
+html = browser.page_source
+soup = BeautifulSoup(html, 'lxml')
+
+find_date = soup.select("#app > div.view > div > div.search-result.result-detail-content > div.track-detail > div.track-center > div > div > div.track-result-content > div > div.track-pandect > ul > li.result-li-three > div > div > div.text-gray")[0].text
+
+print(find_date)
---
+++ track.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -e "env" ]; then
+source env/bin/activate
+fi
+TRACKINGNUMBER=$1
+res=`python track.py $TRACKINGNUMBER`
+if ! [ -e "result" ]; then
+touch result
+fi
+
+res1=`cat result`
+
+if [ "$res" = "$res1" ]; then
+echo "No Update"
+else
+echo $res
+fi
+
+echo $res > result
+
+deactivate
